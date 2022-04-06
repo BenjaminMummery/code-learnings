e17e02153c2d6a529bacc138c4662db39b48f79f
Benjamin Mummery <benjamin.mummery@hotmail.co.uk>
2022-04-06 14:19:10 +0100

    moved hook logs to subdir to separate them from logs generated via other means

--- scripts/clear_logs.sh
+++ scripts/clear_logs.sh
@@ -3,6 +3,6 @@
 # Remove log files. Intended to be executed at the beginning of a pre-commit run
 # to remove leftover logs from previous runs.
 
-if compgen -G "./logs/*.log" > /dev/null; then
-    rm logs/*.log
+if compgen -G "./logs/hooks/*.log" > /dev/null; then
+    rm logs/hooks/*.log
 fi
