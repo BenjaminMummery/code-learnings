4f6845394d88efe568d7fa757b8b29baeef9cf39
Benjamin Mummery <benjamin.mummery@zapatacomputing.com>
2022-07-11 16:19:49 +0100

    docs: Streamline tips and tips_and_tricks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # code-learnings
+
+## Tips and Tricks
+
+The tips_and_tricks directory contains notes on useful habits, shortcuts, and techniques.
--- scripts/clear_logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove log files. Intended to be executed at the beginning of a pre-commit run
-# to remove leftover logs from previous runs.
-
-if compgen -G "./logs/hooks/*.log" > /dev/null; then
-    rm logs/hooks/*.log
-fi
